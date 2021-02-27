f04e1716c76a62018389353c2cfab3e39bc1820b
Anton Proshchenko <0954264@gmail.com>
2021-02-27 18:00:20 +0200

    Added directory path request

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 
 using namespace std;
+using namespace std::filesystem;
 
 int main() {
-    string line;
-    ifstream file;
-    string path = "..\\data\\league1.csv";
-    file.open(path);
-    while (getline(file, line)){
-        cout << line << endl;
+    string directoryName;
+    getline(cin, directoryName);
+
+    string path = "..\\";
+    path += directoryName;
+    path += "\\league1.csv";
+
+    string currentLine;
+    ifstream currentFile;
+    currentFile.open(path);
+
+    while (getline(currentFile, currentLine)){
+        cout << currentLine << endl;
     }
 }
