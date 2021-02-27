621eb9118ff2aabdd9878e641ea3c5f2cdbbddd5
Anton Proshchenko <0954264@gmail.com>
2021-02-27 18:22:38 +0200

    Added reading of all csv files

--- main.cpp
+++ main.cpp
@@ -3,21 +3,25 @@
 #include <filesystem>
 
 using namespace std;
-using namespace std::filesystem;
+namespace fs = std::filesystem;
 
 int main() {
     string directoryName;
     getline(cin, directoryName);
+    string pathToDir = "..\\";
+    pathToDir += directoryName;
 
-    string path = "..\\";
-    path += directoryName;
-    path += "\\league1.csv";
+    for (const auto &entry: fs::directory_iterator(pathToDir)){
+        const fs::path &currentPath = entry.path();
+        string fileExtension = currentPath.extension().string();
+        if (fileExtension != ".csv") continue;
 
-    string currentLine;
-    ifstream currentFile;
-    currentFile.open(path);
+        string currentLine;
+        ifstream currentFile;
+        string stringFilePath = currentPath.string();
 
-    while (getline(currentFile, currentLine)){
+        currentFile.open(stringFilePath);
+        getline(currentFile, currentLine);
         cout << currentLine << endl;
     }
 }
