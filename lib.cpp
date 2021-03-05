aeb54788f1d63518c9ec2b807cac74f73b4b0cdd
Snakeheart404 <polina.glazunova@yahoo.com>
2021-03-05 18:36:01 +0200

    : fix

--- lib.cpp
+++ lib.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <fstream>
 #include <filesystem>
 
@@ -9,30 +10,24 @@ const string csv_delimiter = ",";
 const string scoreDelimiter = ":";
 const int matchesInLeague = 10;
 
-enum MATCH_RESULT {
-    WIN,
-    LOSE,
-    DRAW
-};
-
-void process_entry(const filesystem::directory_entry &entry);
-bool is_csv(const fs::directory_entry &entry);
-int getTeamCount(const string &path);
+void process_entry(const filesystem::directory_entry& entry);
+bool is_csv(const fs::directory_entry& entry);
+int getTeamCount(const string& path);
 
 /** Takes a string with team info as a parameter.
     Returns the team's final score.
 */
-int process_teamResult(const string &teamLine);
-string getTeamName(const string &teamLine);
+int process_teamResult(const string& teamLine);
+string getTeamName(const string& teamLine);
 
 /** Takes a string with result of a match as a parameter.
     Returns the points the team got for the match.
 */
-int process_matchResult(const string &matchString);
-int teamMatchScore(const string &matchString);
-int opponentMatchScore(const string &matchString);
-MATCH_RESULT getMatchResult(int teamScore, int opponentScore);
-int getPoints(MATCH_RESULT result);
+int process_matchResult(const string& matchString);
+int teamMatchScore(const string& matchString);
+int opponentMatchScore(const string& matchString);
+string getMatchResult(int teamScore, int opponentScore);
+int getPoints(string result);
 
 string requestDirectoryPath() {
     string dirName;
@@ -43,7 +38,7 @@ string requestDirectoryPath() {
     return pathToDir;
 }
 
-void process_entry(const fs::directory_entry &entry) {
+void process_entry(const fs::directory_entry& entry) {
     if (!is_csv(entry)) return;
 
     string currentLine;
@@ -65,13 +60,13 @@ void process_entry(const fs::directory_entry &entry) {
     cout << endl;
 }
 
-bool is_csv(const fs::directory_entry &entry) {
-    const fs::path &path = entry.path();
+bool is_csv(const fs::directory_entry& entry) {
+    const fs::path& path = entry.path();
     string fileExtension = path.extension().string();
     return fileExtension == ".csv";
 }
 
-int getTeamCount(const string &path) {
+int getTeamCount(const string& path) {
     ifstream file;
     //the first line is the team count, we get it now
     file.open(path);
@@ -84,7 +79,7 @@ int getTeamCount(const string &path) {
     return intCount;
 }
 
-int process_teamResult(const string &teamLine) {
+int process_teamResult(const string& teamLine) {
     string teamName = getTeamName(teamLine);
     cout << teamName << endl;
 
@@ -105,24 +100,24 @@ int process_teamResult(const string &teamLine) {
     return finalTeamScore;
 }
 
-string getTeamName(const string &teamLine) {
+string getTeamName(const string& teamLine) {
     int teamNameEndInd = teamLine.find_first_of(csv_delimiter);
     string teamName = string(teamLine, 0, teamNameEndInd);
     return teamName;
 }
 
-int process_matchResult(const string &matchString) {
+int process_matchResult(const string& matchString) {
     int teamScore = teamMatchScore(matchString);
     int opponentScore = opponentMatchScore(matchString);
 
-    MATCH_RESULT result = getMatchResult(teamScore, opponentScore);
+    string result = getMatchResult(teamScore, opponentScore);
     int pointsGot = getPoints(result);
     cout << teamScore << ":" << opponentScore << " " << result << endl;
 
     return pointsGot;
 }
 
-int teamMatchScore(const string &matchString) {
+int teamMatchScore(const string& matchString) {
     int delimiterInd = matchString.find_first_of(scoreDelimiter);
     string teamScoreString = string(matchString, 0, delimiterInd);
 
@@ -130,7 +125,7 @@ int teamMatchScore(const string &matchString) {
     return scoreInt;
 }
 
-int opponentMatchScore(const string &matchString) {
+int opponentMatchScore(const string& matchString) {
     int delimiterInd = matchString.find_first_of(scoreDelimiter);
 
     int scoreStartInd = delimiterInd + 1;
@@ -141,24 +136,27 @@ int opponentMatchScore(const string &matchString) {
     return scoreInt;
 }
 
-MATCH_RESULT getMatchResult(int teamScore, int opponentScore) {
-    MATCH_RESULT result;
+string getMatchResult(int teamScore, int opponentScore) {
+    string result = "LOSE";
     if (teamScore > opponentScore) {
-        result = WIN;
-    } else if (teamScore < opponentScore) {
-        result = LOSE;
-    } else {
-        result = DRAW;
+        result = "WIN";
+    }
+    else if (teamScore < opponentScore) {
+        result = "LOSE";
+    }
+    else {
+        result = "DRAW";
     }
 
     return result;
 }
 
-int getPoints(MATCH_RESULT result) {
+int getPoints(string result) {
     int points = 0;
-    if (result == WIN) {
+    if (result == "WIN") {
         points = 3;
-    } else if (result == DRAW) {
+    }
+    else if (result == "DRAW") {
         points = 1;
     }
 
