647f1f5f616303a024ae0997acf1c35ac3178dbb
Snakeheart404 <polina.glazunova@yahoo.com>
2021-03-06 03:19:19 +0200

    added output

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <filesystem>
 #include <vector>
+#include <fstream>
 #include "lib.h"
 
 using namespace std;
 namespace fs = std::filesystem;
+ofstream outFile;
 
 int main() {
     string directoryPath = requestDirectoryPath();
@@ -12,7 +14,14 @@ int main() {
     for (const auto &entry: fs::directory_iterator(directoryPath)) {
         process_entry(entry, leagueTable);
     }
-    outputTable(leagueTable);
     sortTable(leagueTable);
-    outputTable(leagueTable);
+    ofstream outFile("results.csv");
+    if (!outFile) {
+        cout << "Cannot open file" << endl;
+        return 0;
+    }
+    for (int i = 0; i < leagueTable.size(); i++) {
+        outFile << leagueTable[i] << endl;
+    }
+    outFile.close();
 }
